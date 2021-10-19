c0fa7b9e1ec44234bc6c368eb936ad8414a3f577
Ali Madooei <alimadooei@gmail.com>
2021-10-18 23:57:28 -0400

    Step 6: Search Component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,14 @@
-function Search () {
-  return <>Search</>;
+import SearchBar from "material-ui-search-bar";
+
+function Search(props) {
+  const { query, updateQuery } = props;
+  return (
+    <SearchBar 
+      value={query} 
+      onChange={(newValue) => updateQuery(newValue)} 
+      onCancelSearch={() => updateQuery("")} 
+    />
+  );
 }
 
 export default Search;
