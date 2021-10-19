5dce2146048aaae158a9cd9722c3f60fec6735a8
Ali Madooei <alimadooei@gmail.com>
2021-10-18 21:39:22 -0400

    Starting part-3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Container } from "@material-ui/core";
+import DisplayNotes from "./pages/DisplayNotes";
+import UpsertNote from "./pages/UpsertNote";
+import { Route, Switch } from "react-router";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: "5c83c052-60da-425f-a302-9d4735a9d6ae",
+          title:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut.",
+          text: "In orci urna, feugiat sed est id, sodales pharetra nisl. Etiam mollis vehicula imperdiet. In iaculis convallis leo, a pretium erat rutrum in. Ut lacus sem, lobortis id lacinia nec, imperdiet at quam. Etiam ut risus ligula. Mauris ac enim ut purus iaculis lobortis. Donec euismod vel risus ac blandit. Sed in odio enim. Aliquam dolor nibh, luctus a fermentum dignissim, ornare nec enim. Sed posuere in arcu eu ultricies.",
+        },
+        {
+          id: "8fd80679-73ff-4425-ae6d-cffe4463d64f",
+          title: "Cras bibendum libero eu viverra tristique.",
+          text: "Sed et elit id ex lobortis dictum sed vel ex. Mauris id massa interdum, maximus nisl sed, dignissim magna. Donec eu leo varius, malesuada mauris fringilla, porta erat. Nullam consequat auctor tincidunt. Mauris eget lacus ex. Aliquam nibh sem, placerat at nunc at, fringilla auctor dolor. Aliquam eros elit, tempor ut erat eget, interdum porta lacus. Suspendisse in bibendum enim.",
+        },
+        {
+          id: "95747b61-c935-45e9-a156-325307bade96",
+          title:
+            "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.",
+          text: "Praesent eleifend, lectus non molestie dictum, arcu sapien accumsan eros, sodales dapibus dolor lacus in dolor. Fusce blandit augue condimentum eros luctus ullamcorper. Praesent hendrerit nunc a augue tempor finibus. Morbi ultricies lectus ac risus hendrerit, a aliquam ante tincidunt. Suspendisse viverra iaculis consequat. Nam nec consectetur diam. Cras porta metus in nibh facilisis interdum. Aenean lobortis feugiat enim quis molestie. Suspendisse ultrices bibendum volutpat. Praesent et orci est. Pellentesque ut fringilla nibh. Donec vel pretium nisl. Praesent varius, magna sit amet mollis rutrum, urna lacus rutrum magna, in cursus lectus massa id lorem. Etiam risus enim, fringilla sit amet lectus at, condimentum maximus nulla.",
+        },
+      ],
+    };
+  }
+
+  deleteNote = (note) => {
+    this.setState((state) => {
+      return {
+        notes: state.notes.filter((n) => n.id !== note.id),
+      };
+    });
+  };
+
+  addNote = (note) => {
+    this.setState((state) => {
+      return {
+        notes: [...state.notes, Object.assign(note, { id: uuidv4() })],
+      };
+    });
+  };
+
+  editNote = (note) => {
+    this.setState((state) => {
+      return {
+        notes: state.notes.map((n) => (n.id === note.id ? note : n)),
+      };
+    });
+  };
+
+  render() {
+    const { notes } = this.state;
+    return (
+      <Container>
+        <Switch>
+          <Route exact path="/">
+            <DisplayNotes notes={notes} deleteNote={this.deleteNote} />
+          </Route>
+          <Route path="/add">
+            <UpsertNote upsertNote={this.addNote} />
+          </Route>
+          <Route path="/edit">
+            <UpsertNote upsertNote={this.editNote} />
+          </Route>
+        </Switch>
+      </Container>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Note.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Button,
+} from "@material-ui/core";
+import { ExpandLess, ExpandMore, Delete, Edit } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+
+class Note extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  render() {
+    const { note, deleteNote } = this.props;
+    const { open } = this.state;
+
+    return (
+      <>
+        <ListItem>
+          <ListItemIcon onClick={this.handleClick}>
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItemIcon>
+          <ListItemText primary={note.title} />
+          <ListItemIcon>
+            <Link
+              to={{
+                pathname: "/edit",
+                search: `?id=${note.id}`,
+                state: { title: note.title, text: note.text, id: note.id },
+              }}
+            >
+              <Button>
+                <Edit />
+              </Button>
+            </Link>
+          </ListItemIcon>
+          <ListItemIcon>
+            <Button onClick={() => deleteNote(note)}>
+              <Delete />
+            </Button>
+          </ListItemIcon>
+        </ListItem>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItemText secondary={note.text} />
+          </List>
+        </Collapse>
+      </>
+    );
+  }
+}
+
+export default Note;
